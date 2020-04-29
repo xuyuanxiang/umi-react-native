edff80802731bd93df79ce184cd827732fae32f5
徐远翔 <xuyuanxiang@shouqianba.com>
2020-04-29 15:16:13 +0800

    feat: 支持下列umi配置：
      + chainWebpack
      + extraBabelPlugins
      + extraBabelPresets
    
    新增`umi-plugin-antd-react-native`用于集成`@ant-design/react-native`。

--- packages/preset/src/plugins/generateFiles/babelConfig.ts
+++ packages/preset/src/plugins/generateFiles/babelConfig.ts
@@ -1,40 +1,73 @@
 import { IApi } from '@umijs/types';
+import { getBabelPresetOpts, getBabelOpts } from '@umijs/bundler-utils';
 import { dirname } from 'path';
 
-const CONTENT = `module.exports = {
-  presets: ['{{{ presetPath }}}'],
-};
+const CONTENT = `module.exports = {{{ babelConfig }}};
 
 `;
 
+const METRO_PRESETS_REG = /metro-react-native-babel-preset/;
+const UMI_PRESET_REG = /(@umijs\/)?babel-preset-umi/;
+
+interface IImportPluginOpts {
+  libraryName: string;
+  libraryDirectory?: string;
+  style?: boolean;
+  camel2DashComponentName?: boolean;
+}
+
 export default (api: IApi) => {
   const {
-    utils: { winPath, Mustache },
+    utils: { winPath, Mustache, lodash },
   } = api;
 
   api.modifyBabelOpts((opts) => {
-    api.logger.info('modifyBabelOpts:', {
-      ...opts,
-      presets: [
-        ...opts.presets,
-        winPath(dirname(require.resolve('@haul-bundler/babel-preset-react-native/package.json'))),
-      ],
-    });
     return {
       ...opts,
       presets: [
-        ...opts.presets,
+        // 剔除：@umijs/babel-preset-umi 和 metro-react-native-babel-preset
+        ...opts.presets.filter((it) => !METRO_PRESETS_REG.test(it) && !UMI_PRESET_REG.test(it)),
         winPath(dirname(require.resolve('@haul-bundler/babel-preset-react-native/package.json'))),
       ],
     };
   });
 
-  // api.onGenerateFiles(async () => {
-  //   api.writeTmpFile({
-  //     path: 'babel.config.js',
-  //     content: Mustache.render(CONTENT, {
-  //       presetPath: winPath(dirname(require.resolve('@haul-bundler/babel-preset-react-native/package.json'))),
-  //     }),
-  //   });
-  // });
+  api.onGenerateFiles(async () => {
+    const env = api.env === 'production' ? 'production' : 'development';
+    let presetOpts = getBabelPresetOpts({
+      config: api.config,
+      env,
+    });
+    presetOpts = await api.applyPlugins({
+      type: api.ApplyPluginsType.modify,
+      key: 'modifyBabelPresetOpts',
+      initialValue: presetOpts,
+    });
+    let babelOpts = getBabelOpts({
+      cwd: api.cwd,
+      config: api.config,
+      presetOpts,
+    });
+    babelOpts = await api.applyPlugins({
+      type: api.ApplyPluginsType.modify,
+      key: 'modifyBabelOpts',
+      initialValue: babelOpts,
+    });
+    if (presetOpts.import) {
+      /**
+       * presetOpts: 即@umijs/babel-preset-umi的配置参数。
+       * 目前只有@umijs/plugin-antd用到 import 选项，其他配置赞不支持。避免和haul的babel配置冲突
+       */
+      const plugins: [string, any, (string | undefined)?][] = presetOpts.import.map((importOpts: IImportPluginOpts) => {
+        return [require.resolve('babel-plugin-import'), importOpts, importOpts.libraryName];
+      });
+      babelOpts.plugins.push(...plugins);
+    }
+    api.writeTmpFile({
+      path: 'babel.config.js',
+      content: Mustache.render(CONTENT, {
+        babelConfig: JSON.stringify(lodash.pick(babelOpts, ['presets', 'plugins']), null, 2),
+      }),
+    });
+  });
 };
--- packages/runtime/README.md
+++ packages/runtime/README.md
@@ -6,10 +6,12 @@
 
 当用户启用[dynamicImport](https://umijs.org/config#dynamicimport)，并且没有实现自定义的 Loading 组件时，在 RN 中运行会报错，这里做了 Monkey Patch。
 
-`@umijs/runtime` 导出的是`react-router-dom`，在 RN 中需要换为：`react-router-native`。
+另外，`@umijs/runtime` 导出的是`react-router-dom`，在 RN 中需要换为：`react-router-native`。
 
 `react-router-dom`和`react-router-native`大部分组件都导出自`react-router`，但存在以下差异：
 
 - `react-router-native`中没有`NavLink`组件。
 - `react-router-dom`中没有`BackButton`和`AndroidBackButton`组件。
 - 二者的`Link`组件是不同的实现。
+
+了解更多详情：[README](https://github.com/xuyuanxiang/umi-react-native#readme)
