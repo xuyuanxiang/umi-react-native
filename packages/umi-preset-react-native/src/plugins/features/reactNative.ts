87756538d6d5eac483f0143eeb3b2f320d0f6f57
徐远翔 <xuyuanxiang@shouqianba.com>
2020-05-14 20:01:13 +0800

    fix: add react-router-config

--- packages/umi-preset-react-native/src/plugins/features/reactNative.ts
+++ packages/umi-preset-react-native/src/plugins/features/reactNative.ts
@@ -94,7 +94,6 @@ export default (api: IApi) => {
       path: 'react-router-native',
     },
     { name: 'react-router-native', path: winPath(dirname(require.resolve('react-router-native/package.json'))) },
-    { name: 'react-router-config', path: winPath(dirname(require.resolve('react-router-config/package.json'))) },
   ]);
 
   // 启动时检查
