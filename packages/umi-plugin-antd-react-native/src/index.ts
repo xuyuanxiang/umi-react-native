de8e7c3ad4a2c397a8dfd45c6112315693ba9d39
徐远翔 <xuyuanxiang@shouqianba.com>
2020-05-20 19:24:28 +0800

    feat: umi-plugin-antd-react-native 支持在 expo 中为 @ant-design/react-native [链接字体图标](https://rn.mobile.ant.design/docs/react/introduce-cn#%E9%93%BE%E6%8E%A5%E5%AD%97%E4%BD%93%E5%9B%BE%E6%A0%87)

--- packages/umi-plugin-antd-react-native/src/index.ts
+++ packages/umi-plugin-antd-react-native/src/index.ts
@@ -3,16 +3,46 @@ import { join } from 'path';
 
 const runtimeTpl = `import React from 'react';
 import {Provider} from '@ant-design/react-native';
-const props = {{#theme}}{theme: {{{ theme }}}}{{/theme}}{{^theme}}null{{/theme}};
+{{#isExpo}}
+import {dynamic} from 'umi';
+import {AppLoading} from 'expo';
+import {loadAsync} from 'expo-font';
 
+export function rootContainer(container) {
+  return React.createElement(dynamic({
+    loading: AppLoading,
+    loader: async () => {
+      await loadAsync(
+        'antoutline',
+        // eslint-disable-next-line
+        require('@ant-design/icons-react-native/fonts/antoutline.ttf')
+      );
+      await loadAsync(
+        'antfill',
+        // eslint-disable-next-line
+        require('@ant-design/icons-react-native/fonts/antfill.ttf')
+      );
+      
+      return () => React.createElement(
+        Provider,
+        {{#theme}}{theme: {{{ theme }}}}{{/theme}}{{^theme}}null{{/theme}},
+        container,
+      );
+    },
+  }));
+}
+
+{{/isExpo}}
+{{^isExpo}}
 export function rootContainer(container) {
   return React.createElement(
     Provider,
-    props,
+    {{#theme}}{theme: {{{ theme }}}}{{/theme}}{{^theme}}null{{/theme}},
     container,
   );
 }
 
+{{/isExpo}}
 `;
 
 // const exportsTpl = `
@@ -35,6 +65,7 @@ export default (api: IApi) => {
       path: 'antd-react-native/runtime.ts',
       content: api.utils.Mustache.render(runtimeTpl, {
         theme: JSON.stringify(api.config.theme, null, 2),
+        isExpo: Boolean(api.config.expo),
       }),
     });
     // api.writeTmpFile({
@@ -44,5 +75,20 @@ export default (api: IApi) => {
     //       api.config.theme && typeof api.config.theme === 'object' ? JSON.stringify(api.config.theme, null, 2) : null,
     //   }),
     // });
+  });
+
+  api.onStart(() => {
+    if (api.config.expo) {
+      if (api.pkg.dependencies && api.pkg.dependencies['expo-font']) {
+        return;
+      }
+      if (api.pkg.devDependencies && api.pkg.devDependencies['expo-font']) {
+        return;
+      }
+      api.logger.error(
+        '@ant-design/react-native的字体图标资源文件依赖"expo-font"，请执行：`expo install expo-font`将其安装至 RN 工程本地。',
+      );
+      throw new TypeError('"expo-font" 未安装！');
+    }
   });
 };
