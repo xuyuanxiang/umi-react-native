07c1757651e5be40c4c671c6bf7fe9a6ff5cec50
徐远翔 <xuyuanxiang@shouqianba.com>
2020-06-01 17:28:55 +0800

    ci: fix android test

--- example/android.wdio.config.js
+++ example/android.wdio.config.js
@@ -11,7 +11,7 @@ exports.config = {
     {
       maxInstances: 1,
       platformName: 'Android',
-      platformVersion: 'android-28',
+      platformVersion: '10.0',
       deviceName: 'TestingAVD',
       app: path.join(__dirname, 'android/app/build/outputs/apk/debug/app-debug.apk'),
       automationName: 'UiAutomator2',
